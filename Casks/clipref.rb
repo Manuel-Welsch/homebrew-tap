4d386db6ac36db4a56e4ec86de95e36acd350856
Manuel Welsch <mail@manuelwelsch.de>
2026-06-04 13:23:13 +0200

    feat: add clipref cask (0.1.0)

---
+++ Casks/clipref.rb
@@ -0,0 +1,15 @@
+cask "clipref" do
+  version "0.1.0"
+  sha256 "547a507a5a8bf155fee8b4cc05b12538c8aa87893597d0cf744deb39f9604638"
+
+  url "https://github.com/Manuel-Welsch/ClipRef/releases/download/v#{version}/ClipRef-#{version}.zip"
+  name "ClipRef"
+  desc "Menu-bar app that saves the clipboard to a file and gives you an @-path for Claude Code"
+  homepage "https://github.com/Manuel-Welsch/ClipRef"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ClipRef.app"
+
+  zap trash: "~/Library/Preferences/de.manuelwelsch.ClipRef.plist"
+end
